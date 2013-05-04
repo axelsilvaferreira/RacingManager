247fc0a10d984e49df2a963fc5b72e1a6673dab5
Axel Ferreira <axelferreira@me.com>
2013-05-04 19:29:37 +0100

    Criação da Classe Veiculo

--- RacingManager/src/racingmanager/Veiculo.java
+++ RacingManager/src/racingmanager/Veiculo.java
@@ -5,7 +5,6 @@
 package racingmanager;
 
 import java.io.Serializable;
-import java.util.*;
 
 /**
  * Superclass abstrata que representa um Veiculo
@@ -18,32 +17,30 @@ public abstract class Veiculo implements Serializable
 {   // Versão de serialização
     private static final long serialVersionUID = 1L;
     
-    
         // Estrutura de dados
         private String marca;        // Marca
         private String modelo;       // Modelo
         private String[] pilotos;    // Estrutura de Pilotos
         private boolean hibrido;     // V - Hibrido / F - Não Hibrido
-        private String classe;       // Classe do Veiculo
+        //private String categoria;       // Classe do Veiculo
    
 
     /**
-     * Construtor Default de objectos da classe Veiculo
+     * Construtor Default de objectos da categoria Veiculo
      */
     public Veiculo() 
     {   // initialização das variáveis de instância
         this.marca       = "";
         this.modelo      = "";
-        this.pilotos     = new String[2];
+        this.pilotos     = new String[2]; // ****** Sera necessario inicializar?
         this.hibrido     = false;
-        this.classe      = "";
+        this.categoria   = "";
     }
     
     /**
-     * Construtor da classe Veiculo com argumentos
+     * Construtor da categoria Veiculo com argumentos
      */
-    @SuppressWarnings("empty-statement")
-    public Veiculo(String marca, String modelo, String p1, String p2, boolean h, String classe) 
+    public Veiculo(String marca, String modelo, String p1, String p2, boolean h, String categoria) 
     {   // initialização das variáveis de instância
         this.marca       = marca;
         this.modelo      = modelo;
@@ -51,11 +48,11 @@ public abstract class Veiculo implements Serializable
         this.pilotos[0]  = p1;
         this.pilotos[1]  = p2;
         this.hibrido     = h;
-        this.classe      = classe;
+       // this.categoria   = categoria;
     }
     
     /**
-     * Construtor da classe Veiculo apartir de um veiculo
+     * Construtor da categoria Veiculo apartir de um veiculo
      */
     public Veiculo(Veiculo v) 
     {   // initialização das variáveis de instância
@@ -65,28 +62,24 @@ public abstract class Veiculo implements Serializable
         this.pilotos[0]  = v.getPiloto(1);
         this.pilotos[1]  = v.getPiloto(2);
         this.hibrido     = v.getHibrido();
-        this.classe      = getClasse();
+       // this.categoria   = v.getCategoria();
     }
 
-    public String getMarca() {
-        return marca;
-    }
+    public String getMarca() 
+    {   return marca;  }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+    public void setMarca(String marca) 
+    {   this.marca = marca;  }
 
-    public String getModelo() {
-        return modelo;
-    }
+    public String getModelo() 
+    {   return modelo;   }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
+    public void setModelo(String modelo) 
+    {   this.modelo = modelo;  }
     
-    /* Metodo que devolve o piloto
+    /** Metodo que devolve o piloto
      * @param i numero do piloto 1 / 2.
-     * @return o Nome do Piloto correspondente ao indice.
+     * @return O Nome do Piloto correspondente ao indice.
      */
     public String getPiloto(int i) 
     { return this.pilotos[i-1]; }
@@ -99,35 +92,61 @@ public abstract class Veiculo implements Serializable
       int i;
       
       for (i=0;i<2;i++)
-      { p[i] = this.pilotos[i];}
+      { p[i] = this.pilotos[i];}            // ***** VER SE È NECESSARIO
        
       return p;
      */
         return this.pilotos;
     }
     
-    /*Metodo que altera os pilotos
-     * 
+    public boolean getHibrido()
+    {   return this.hibrido; }
+    
+    public void setPilotos(String[] pilotos) 
+    {   this.pilotos = pilotos;   }
+
+    public boolean isHibrido() 
+    {   return hibrido;  }
+
+    public void setHibrido(boolean hibrido) 
+    {   this.hibrido = hibrido;   }
+/*
+    public String getCategoria() 
+    {   return categoria;  }
+
+    public void setCategoria(String categoria) 
+    {   this.categoria = categoria;  }
+ */   
+    /**
+     * Metodo abstrato que calcula o tempo da proxima volta para um veiculo.
+     * Este método tem que ser implementado nas subclasses de cada categoria de veiculo.
+     * @return tempo em segundos da proxima volta
      */
-    public void setPilotos(String[] pilotos) {
-        this.pilotos = pilotos;
-    }
-
-    public boolean isHibrido() {
-        return hibrido;
-    }
-
-    public void setHibrido(boolean hibrido) {
-        this.hibrido = hibrido;
-    }
-
-    public String getClasse() {
-        return classe;
-    }
-
-    public void setClasse(String classe) {
-        this.classe = classe;
-    }
+    public abstract int tempoProximaVolta();
+    
+    
+    
+    @Override
+    public abstract String toString();
+    /*{ StringBuilder s = new StringBuilder;
+        s.append(Veiculo);
+        s.append(this.getMarca());
+        s.append("\n");
+        return s.toString();
+    }*/
+    
+    /**
+     * Metodo que compara o veiculo com outro.
+     * @return Verdeiro ou Falso.
+     */
+    public abstract boolean equals();
+    
+    
+    
+    
+    
+    
+    
     
     
     
