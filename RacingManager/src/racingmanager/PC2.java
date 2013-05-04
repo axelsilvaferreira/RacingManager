8147635b922e9085907325253001ed362d3dfa3b
Axel Ferreira <axelferreira@me.com>
2013-05-04 19:29:25 +0100

    Criação da Classe PC2

---
+++ RacingManager/src/racingmanager/PC2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racingmanager;
+
+import java.io.Serializable;
+
+/**
+ * Classe que repsresenta um veiculo do tipo PC2
+ * 
+ * @author axelferreira
+ * 
+ * @version 1.0
+ */
+public class PC2 extends Veiculo implements Serializable 
+{
+    private static float fiabilidade;
+
+    @Override
+    public int tempoProximaVolta() 
+    {   throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String toString() 
+    {   throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean equals() 
+    {   throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+    
+}
