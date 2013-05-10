64515a9a44e652f963fb47eca02487a68ba8b759
Axel Ferreira <axelferreira@me.com>
2013-05-10 19:38:54 +0100

    Continuação da classe GT

--- RacingManager/src/racingmanager/GT.java
+++ RacingManager/src/racingmanager/GT.java
@@ -19,9 +19,33 @@ public class GT extends Veiculo implements Serializable
     
     private static float fiabilidade;
 
+    
+    
+    
+    
+    public GT() 
+    {
+        
+    }
+
+    public GT(String marca, String modelo, String p1, String p2, boolean h, String categoria) 
+    {
+        super(marca, modelo, p1, p2, h, categoria);
+    }
+
+    public GT(Veiculo v) 
+    {
+        super(v);
+    }
+    
+    
+    
+    
+
     @Override
     public int tempoProximaVolta() 
-    {   throw new UnsupportedOperationException("Not supported yet.");
+    {   
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
