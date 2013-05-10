201ad4b15d0a0d6633812b7ac91969a11dfbcec7
Tiago João Alves de Sousa <tjalvessousa@gmail.com>
2013-05-10 19:14:55 +0100

    Classe piloto criada

---
+++ RacingManager/src/racingmanager/Piloto.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racingmanager;
+
+import java.io.Serializable;
+
+/**
+ * Superclass abstrata que representa um Veiculo
+ * 
+ * @author axelferreira
+ * 
+ * @version 1.0
+ */
+public abstract class Piloto implements Serializable
+{   // Versão de serialização
+    private static final long serialVersionUID = 1L;
+    
+    //Estrutura de dados
+    
+    private String nome;
+    private String nacionalidade;
+    private Integer skill;
+    private Integer wSkill;
+    private Integer vitorias;
+    private String equipa;
+
+    public Piloto(String nome, String nacionalidade, Integer Skill, Integer wSkill, Integer vitorias, String equipa) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.skill = Skill;
+        this.wSkill = wSkill;
+        this.vitorias = vitorias;
+        this.equipa = equipa;
+    }
+   
+       public Piloto() {
+        this.nome = "";
+        this.nacionalidade = "";
+        this.skill = 0;
+        this.wSkill = 0;
+        this.vitorias = 0;
+        this.equipa = "";
+    }
+       public Piloto (Piloto p) {
+        this.nome = p.getNome(); 
+        this.nacionalidade = p.getNacionalidade();
+        this.skill = p.getSkill();
+        this.wSkill = p.getwSkill();
+        this.vitorias = p.getVitorias();
+        this.equipa = this.getEquipa();
+        
+       
+       }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public Integer getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Integer Skill) {
+        this.skill = Skill;
+    }
+
+    public Integer getwSkill() {
+        return wSkill;
+    }
+
+    public void setwSkill(Integer wSkill) {
+        this.wSkill = wSkill;
+    }
+
+    public Integer getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(Integer vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public String getEquipa() {
+        return equipa;
+    }
+
+    public void setEquipa(String equipa) {
+        this.equipa = equipa;
+    }
+
+ 
+    
+    
+    
+    
+}
